0861843acc58162232e731c1e636857aed15b27d
Maccrey <m01071630214@gmail.com>
2025-01-21 17:23:43 +0900

    더보고/ 라이센스 Icon 및 버튼 추가

--- lib/src/views/more/widgets/logout_btn_widget.dart
+++ lib/src/views/more/widgets/logout_btn_widget.dart
@@ -2,20 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../providers/auth/auth_provider.dart';
+import '../views/license/licenses_screen.dart';
 
 class LogoutButton extends ConsumerWidget {
   const LogoutButton({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ListTile(
-      dense: true,
-      contentPadding: EdgeInsets.zero,
-      leading: const Icon(Icons.logout),
-      title: const Text('로그아웃'),
-      onTap: () async {
-        ref.read(authProvider.notifier).logout();
-      },
+    return Column(
+      children: [
+        ListTile(
+          dense: true,
+          contentPadding: EdgeInsets.zero,
+          leading: const Icon(Icons.logout),
+          title: const Text('로그아웃'),
+          onTap: () async {
+            ref.read(authProvider.notifier).logout();
+          },
+        ),
+        ListTile(
+          dense: true,
+          contentPadding: EdgeInsets.zero,
+          leading: const Icon(Icons.description),
+          title: const Text('라이센스'),
+          onTap: () async {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => OssLicensesScreen(),
+            ));
+          },
+        ),
+      ],
     );
   }
 }
